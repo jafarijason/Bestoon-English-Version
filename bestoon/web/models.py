1d5e8a05da22dc1328bbb55741de232fa008c489
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-04-19 23:58:16 -0400

    1212

--- bestoon/web/models.py
+++ bestoon/web/models.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
 
 # Create your models here.
